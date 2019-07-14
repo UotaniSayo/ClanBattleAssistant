45a865a57f2a1ee85223be56c9dfd6288c32bc5b
Xinyou Han <b4baker@gmail.com>
2019-07-15 08:53:56 +0900

    简单注释，调整bossIndex，调整第二次检查内容
    阶段数不能超过3，也不能小于1，编号同理

--- plugins/clanBtl.py
+++ plugins/clanBtl.py
@@ -3,30 +3,41 @@ from nonebot.permission import GROUP, PRIVATE_FRIEND
 import re
 import csv
 
+#指令：查询boss信息，返回boss的双防
 @on_command('askBoss', aliases=('查boss', '查Boss', '查BOSS'), permission=GROUP | PRIVATE_FRIEND, only_to_me=True)
 async def askBoss(session: CommandSession):
+	#获取boss名字，如果没有则为 None
 	bossName = session.state['bossName'] if 'bossName' in session.state else None
+	#没有输入boss名字时，提示输入
 	if not bossName:
 		bossName = session.get('bossName', prompt='输入想要查询的boss，阶段数和编号用半角数字')
 	bossLocStr = re.findall('[0-9]', bossName)
+	
+	#boss名字第一次检查，确认有两个数字
 	if len(bossLocStr) != 2:
 		await session.send('guna!')
 		return
 	bossLoc = [int(bossLocStr[0]), int(bossLocStr[1])]
-	bossIndex = 5*(bossLoc[0]-1)+bossLoc[1]-1
-	if bossLoc[0]>3 or bossLoc[1]>5:
+	bossIndex = 5*(bossLoc[0]-1)+bossLoc[1]
+	
+	#boss名字第二次检查，确认阶段数和编号未超出范围
+	if bossLoc[0]>3 or bossLoc[0]<1 or bossLoc[1]>5 or bossLoc[1]<1:
 		await session.send('guna!')
 		return
 	
+	#读取boss信息
 	bossInfo = open('./plugins/boss.csv', 'r')
 	reader = csv.reader(bossInfo)
 	for i, rows in enumerate(reader):
 		if i == bossIndex:
 			row = rows
+	
+	#根据信息生成回复内容
 	bossAns = bossLocStr[0]+'阶段'+bossLocStr[1]+'号：'+'物理防御'+row[2]+'，魔法防御'+row[3]
 	bossInfo.close()
 	await session.send(bossAns)
 	
+#指令解析：查询boss信息
 @askBoss.args_parser
 async def _(session:CommandSession):
 	stripped_arg = session.current_arg_text.strip()
