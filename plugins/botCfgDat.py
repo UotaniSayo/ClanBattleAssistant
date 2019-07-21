998a20e8e2fc08b03f9fd991e940f67b4dccc963
Xinyou Han <left@whu.edu.cn>
2019-07-21 12:17:32 +0900

    Add files via upload

---
+++ plugins/botCfgDat.py
@@ -0,0 +1,14 @@
+#botCfgDat
+#存储bot的配置信息
+
+
+class NekoBot:
+    #公会群号
+    groupNo = 0
+    #公会成员列表
+    clanMbr = []
+    #允许普通成员将自己添加进列表
+    allowMbrSelfAdd = False
+    #测试模式
+    debugMode = True
+    
