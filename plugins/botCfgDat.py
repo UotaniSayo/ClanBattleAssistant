6756c6c3191c518b7f9b6d6a92704eec0bb7b82f
Xinyou Han <b4baker@gmail.com>
2019-07-21 22:53:11 +0900

    文件名修改为唯一ID
    不知道为什么忘了改

--- plugins/botCfgDat.py
+++ plugins/botCfgDat.py
@@ -15,18 +15,18 @@ class NekoBot:
     #允许普通成员将自己添加进列表
     allowMbrSelfAdd = False
     #公会成员群昵称标签
-    mbrTag = '【Mbr】'
+    mbrTag = ''
     #测试模式
     debugMode = True
     
     def saveCfg(self):
-        cfgFile = open('./plugins/{0}.cfg'.format(self.groupNo), 'wb')
+        cfgFile = open('./plugins/{0}.cfg'.format(self.botID), 'wb')
         pickle.dump(self, cfgFile)
         cfgFile.close()
         
     def loadCfg(self):
         try:
-            cfgFile = open('./plugins/{0}.cfg'.format(self.groupNo), 'rb')
+            cfgFile = open('./plugins/{0}.cfg'.format(self.botID), 'rb')
             newBot = pickle.load(cfgFile)
             cfgFile.close()
             return newBot
